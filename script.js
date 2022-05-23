6dcc47e49603836e55ee9b4a3e053343e566382c
masonsau0 <masonsau0@gmail.com>
2022-05-22 20:40:22 -0400

    Fix invalid arguments loop

--- script.js
+++ script.js
@@ -43,11 +43,12 @@ function computerPlay() {
 function userPlay() {
    let userChoice =  prompt("Type rock, paper, or scissors to select move.");
    userChoice = userChoice.toLowerCase();
-   while (userChoice != "rock" || userChoice != "paper" || userChoice != "scissors") {
-       console.log("Invalid move, try again.");
-       userChoice.toLowerCase() =  prompt("Type rock, paper, or scissors to select move.");
-   }
-   return userChoice.toLowerCase();
+    while (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+        console.log("Invalid move, try again.");
+        userChoice =  prompt("Type rock, paper, or scissors to select move.");
+        userChoice = userChoice.toLowerCase();
+     }
+    return userChoice.toLowerCase();
 }
 
 function playRound(userSelection, computerSelection) {
